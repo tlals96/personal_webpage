3673c9d5b0419caf556966acd2981e5d8069bb3a
tlals96 <tlals96@gmail.com>
2021-05-25 16:55:54 +0900

    delete not use Component

--- personal_web/src/App.js
+++ personal_web/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 //import './App.css';
-import { AppBar, Button, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import Dashboard from './dashboard/Dashboard'
 
 const theme = createMuiTheme({
